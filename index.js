faa2b3c68ecd7c025ba61efabff700887c5b88d7
Hiba-Mohamed <hibamoha987@gmail.com>
2024-03-29 18:07:13 -0230

    folder structure, json file completed

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello World! This is index.js file")
+fetch('./hospital.json')
+
+function getTotaNumberOfEmployees(){}
+
+function getTotalNumberOfUnits{}
+
+function getTotalNumberOfBeds{}
